a1ecdf9ff29a89a086217a93a3fd14390fc9bc8e
Nneji Obioma <nnejiobioma@gmail.com>
2023-12-13 07:39:37 +0100

    Added links to social

--- potfolio_project/app/page.tsx
+++ potfolio_project/app/page.tsx
@@ -5,7 +5,7 @@ import skills from './skills/page'
 import Link from 'next/link'
 import React from "react"; 
 import { BsTwitterX } from "react-icons/bs";
-import { FaFacebookF, FaInstagram, FaWhatsapp, FaYoutube } from "react-icons/fa";
+import { FaFacebookF, FaInstagram, FaWhatsapp, FaYoutube, FaGithub, FaLinkedin } from "react-icons/fa";
 
   
 
@@ -17,28 +17,38 @@ export default function Home() {
   <div className='leftSide p-10 my-20 text-9xl font-bold flex flex-col space-y-5 animate-bounce w-20 h-20 '>
    <div className='bg-red-100'>
    <div className='hover:indent-8'>
-    <Link href={"/"}>
+    <Link href={"https://twitter.com/ojnneji"}>
       <BsTwitterX size={80} />
 </Link>
   </div>
   <div className='hover:indent-8'>
-  <Link href={"/"}>
+  <Link href={"https://www.facebook.com/onneji/"}>
       <FaFacebookF size={80} />
 </Link>
   </div>
   <div className='hover:indent-8'>
-  <Link href={"/"}>
+  <Link href={"https://www.instagram.com/ojnneji/"}>
       <FaInstagram size={80} />
 </Link>
   </div>
   <div className='hover:indent-8'>
-  <Link href={"/"}>
+  <Link href={"https://wa.me/message/6SMPLULSD2PPL1"}>
       <FaWhatsapp size={80} />
 </Link>
   </div>
   <div className='hover:indent-8'>
   <Link href={"/"}>
       <FaYoutube size={80} />
+</Link>
+  </div>
+  <div className='hover:indent-8'>
+  <Link href={"https://github.com/nnejiobioma"}>
+      <FaGithub size={80} />
+</Link>
+  </div>
+  <div className='hover:indent-8'>
+  <Link href={"https://www.linkedin.com/in/nneji-obioma"}>
+      <FaLinkedin size={80} />
 </Link>
   </div>
    </div>
