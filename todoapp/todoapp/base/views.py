8f53f31f3edc51f1cf5d676f319356ab8fba47fa
Nneji Obioma <nnejiobioma@gmail.com>
2024-01-11 20:01:55 +0100

    files removed

--- todoapp/todoapp/base/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-# Rendering pages
-# Views take in request or a template
-
-def home(request):
-    return render(request, 'base/home.html')
--- todoapp/todoapp/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./base/templates/base/home.html/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
